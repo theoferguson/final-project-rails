8037f0497d0c28ea8b61523ebea4adcc232bd84e
Theo Ferguson <ferguson.theo@gmail.com>
2021-11-03 20:04:28 -0400

    sign up and in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 
   get "/hello", to: "application#hello_world"
+
+  post "/login", to: "sessions#create"
+
+  get "/me", to: "users#show"
+
+  delete "/logout", to: "sessions#destroy"
 end
